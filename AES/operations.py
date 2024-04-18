f7f4a437ce0615842d01bf9390bbad2ac963dbb1
mahdihaghverdi <mahdihaghverdiliewpl@gmail.com>
2024-04-18 15:20:32 +0330

    implement SubBytes and test them

--- AES/operations.py
+++ AES/operations.py
@@ -7,7 +7,11 @@
 from itertools import islice
 from typing import TypeAlias
 
+import galois
+
 BitMat: TypeAlias = list[list[int]]
+
+GF128 = galois.GF(2, 8, irreducible_poly='x^8 + x^4 + x^3 + x + 1')
 
 
 def batched(iterable, n):
@@ -52,9 +56,9 @@ def _matrix_to_stream(matrix: BitMat) -> str:
 
     Arguments:
         matrix: [[b0, b4, b8, b12],
-                [b1, b5, b9, b13],
-                [b2, b6, b10, b14],
-                [b3, b7, b11, b15]]
+                 [b1, b5, b9, b13],
+                 [b2, b6, b10, b14],
+                 [b3, b7, b11, b15]]
 
     Return:
         'b0b1b2b3...b15'
@@ -66,3 +70,108 @@ def _matrix_to_stream(matrix: BitMat) -> str:
             _stream += f'{bin(row[col])[2:]:0>8}'
         stream += _stream
     return stream
+
+
+############ SB: Sub Bytes ############  # noqa: E266
+sb_mat = GF128(
+    [[1, 0, 0, 0, 1, 1, 1, 1],
+     [1, 1, 0, 0, 0, 1, 1, 1],
+     [1, 1, 1, 0, 0, 0, 1, 1],
+     [1, 1, 1, 1, 0, 0, 0, 1],
+     [1, 1, 1, 1, 1, 0, 0, 0],
+     [0, 1, 1, 1, 1, 1, 0, 0],
+     [0, 0, 1, 1, 1, 1, 1, 0],
+     [0, 0, 0, 1, 1, 1, 1, 1]],
+)
+
+isb_mat = GF128(
+    [[0, 0, 1, 0, 0, 1, 0, 1],
+     [1, 0, 0, 1, 0, 0, 1, 0],
+     [0, 1, 0, 0, 1, 0, 0, 1],
+     [1, 0, 1, 0, 0, 1, 0, 0],
+     [0, 1, 0, 1, 0, 0, 1, 0],
+     [0, 0, 1, 0, 1, 0, 0, 1],
+     [1, 0, 0, 1, 0, 1, 0, 0],
+     [0, 1, 0, 0, 1, 0, 1, 0]],
+)
+
+b = GF128([[1], [1], [0], [0], [0], [1], [1], [0]])
+ib = GF128([[1], [0], [1], [0], [0], [0], [0], [0]])
+
+
+def _sub_byte(byte: int) -> int:
+    """SubByte the integer according to s(x) = ax^(-1)+b"""
+    number = GF128(byte)
+    rev = GF128(number) ** -1 if byte != 0 else 0
+    mat = GF128([[char] for char in reversed(f'{bin(rev)[2:]:0>8}')])
+    result = (sb_mat @ mat) + b
+    num = ''
+    for bit in reversed(result):
+        num += str(bit[0])
+    return int(num, 2)
+
+
+def _isub_byte(byte: int) -> int:
+    number = GF128(byte)
+    mat = GF128([[char] for char in reversed(f'{bin(number)[2:]:0>8}')])
+    result = (isb_mat @ mat) + ib
+    num = ''
+    for bit in reversed(result):
+        num += str(bit[0])
+    rev = int(num, 2)
+    res = GF128(rev) ** -1 if byte != 0 else 0
+    return res
+
+
+def _sub_bytes(input_stream: BitMat) -> BitMat:
+    result = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    for col in range(4):
+        for row in range(4):
+            sb = _sub_byte(input_stream[row][col])
+            result[row][col] = sb
+    return result
+
+
+def _isub_bytes(input_stream: BitMat) -> BitMat:
+    result = [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    for col in range(4):
+        for row in range(4):
+            sb = _isub_byte(input_stream[row][col])
+            result[row][col] = sb
+    return result
+
+
+def sub_bytes(stream: str) -> str:
+    """Sub byte the stream
+
+    Arguments:
+        stream: b1b2b3...b15
+
+    Return: s1s2s3...s15
+    """
+    _is = _stream_to_matrix(stream)
+    return _matrix_to_stream(_sub_bytes(_is))
+
+
+def isub_bytes(stream: str) -> str:
+    """Inverse sub byte the stream
+
+    Arguments:
+        stream: s1s2s3...s15
+
+    Return: b1b2b3...b15
+    """
+    _is = _stream_to_matrix(stream)
+    return _matrix_to_stream(_isub_bytes(_is))
--- Tests/AES/test_operations.py
+++ Tests/AES/test_operations.py
@@ -1,6 +1,4 @@
-import pytest
-
-from AES.operations import _stream_to_matrix, _matrix_to_stream
+from AES.operations import _stream_to_matrix, _matrix_to_stream, sub_bytes, isub_bytes
 
 strm = ''
 for num in range(16):
@@ -20,3 +18,26 @@ def test_stream_to_matrix():
 
 def test_matrix_to_stream():
     assert strm == _matrix_to_stream(mat)
+
+
+numbers = [
+    [0x01, 0x02, 0x03, 0x84],
+    [0x04, 0x05, 0x16, 0xba],
+    [0x17, 0x18, 0x19, 0xec],
+    [0x1d, 0x1e, 0x1f, 0xf7]
+]
+
+answers = [
+    [0x7c, 0x77, 0x7b, 0x5f],
+    [0xf2, 0x6b, 0x47, 0xf4],
+    [0xf0, 0xad, 0xd4, 0xce],
+    [0xa4, 0x72, 0xc0, 0x68]
+]
+
+
+def test_sub_bytes():
+    assert sub_bytes(_matrix_to_stream(numbers)) == _matrix_to_stream(answers)
+
+
+def test_isub_bytes():
+    assert isub_bytes(_matrix_to_stream(answers)) == _matrix_to_stream(numbers)
